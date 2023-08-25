fb3ea3ad5c0b0798d672a18e911409a9e0287ee6
Fran Perez <franperezfolgado@outlook.com>
2023-08-25 19:20:28 +0200

    Added @EnableJpaRepositories in main application class

--- src/main/java/com/fpf/beerdispenser/BeerdispenserApplication.java
+++ src/main/java/com/fpf/beerdispenser/BeerdispenserApplication.java
@@ -2,12 +2,14 @@ package com.fpf.beerdispenser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BeerdispenserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeerdispenserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BeerdispenserApplication.class, args);
+    }
 
 }

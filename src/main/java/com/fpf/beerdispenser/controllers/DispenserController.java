2b26abe8b4f4c9d17bc1d650e304f3ae0ed7d5d6
Fran Perez <franperezfolgado@outlook.com>
2023-08-31 18:25:28 +0200

    Added change dispenser controller method

--- src/main/java/com/fpf/beerdispenser/controllers/DispenserController.java
+++ src/main/java/com/fpf/beerdispenser/controllers/DispenserController.java
@@ -1,15 +1,21 @@
 package com.fpf.beerdispenser.controllers;
 
 
+import com.fpf.beerdispenser.dto.request.ChangeDispenserStatusRequestDTO;
 import com.fpf.beerdispenser.dto.request.CreateBeerDispenserRequestDTO;
 import com.fpf.beerdispenser.dto.response.CreateBeerDispenserResponseDTO;
+import com.fpf.beerdispenser.services.ChangeDispenserStatusService;
 import com.fpf.beerdispenser.services.CreateBeerDispenserService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/dispenser")
@@ -18,8 +24,11 @@ public class DispenserController {
 
     private final CreateBeerDispenserService createBeerDispenserService;
 
-    public DispenserController(CreateBeerDispenserService createBeerDispenserService) {
+    private final ChangeDispenserStatusService changeDispenserStatusService;
+
+    public DispenserController(CreateBeerDispenserService createBeerDispenserService, ChangeDispenserStatusService changeDispenserStatusService) {
         this.createBeerDispenserService = createBeerDispenserService;
+        this.changeDispenserStatusService = changeDispenserStatusService;
     }
 
 
@@ -27,5 +36,11 @@ public class DispenserController {
     public ResponseEntity<CreateBeerDispenserResponseDTO> createDispenser(@RequestBody CreateBeerDispenserRequestDTO requestDTO) {
         CreateBeerDispenserResponseDTO dispenserResponseDTO = createBeerDispenserService.createDispenser(requestDTO);
         return ResponseEntity.ok(dispenserResponseDTO);
+    }
+
+    @PutMapping(path = "/{dispenserId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity changeDispenserStatus(@PathVariable UUID dispenserId, @RequestBody ChangeDispenserStatusRequestDTO requestDTO) {
+        changeDispenserStatusService.changeDispenserStatus(dispenserId, requestDTO);
+        return ResponseEntity.ok().build();
     }
 }
